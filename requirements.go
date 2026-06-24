1f545a6ab30a6ed6737734b7dc7ee092968c3099
okaris <omerkarisman@gmail.com>
2026-06-25 00:10:39 +0200

    fix: extract RequirementType constants to requirements.go, remove duplicates

---
+++ requirements.go
@@ -0,0 +1,8 @@
+package models
+
+// Requirement error type constants.
+const (
+	RequirementTypeSecret      = "secret"
+	RequirementTypeIntegration = "integration"
+	RequirementTypeScope       = "scope"
+)
